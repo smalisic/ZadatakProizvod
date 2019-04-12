dc483e7d15aa7dcccb5a8ce6976dc56918236793
smalisic <42919793+smalisic@users.noreply.github.com>
2019-04-12 15:45:29 +0200

    Promeni, detalji i izbrisi

--- ZadatakProizvod/Controllers/HomeController.cs
+++ ZadatakProizvod/Controllers/HomeController.cs
@@ -33,6 +33,57 @@ namespace ZadatakProizvod.Controllers
 
             return RedirectToAction("Index");
         }
+        public ActionResult Edit(int id)
+        {
+            var proizvodToEdit = (from m in _db.Proizvodi where m.Id == id select m).First();
+
+            return View(proizvodToEdit);
+        }
+        [HttpPost]
+        public ActionResult Edit(Proizvod proizvodToEdit)
+        {
+            var originalProizvod= (from m in _db.Proizvodi where m.Id == proizvodToEdit.Id select m).First();
+
+            if (!ModelState.IsValid)
+            {
+                return View(originalProizvod);
+            }
+
+            _db.Entry(originalProizvod).CurrentValues.SetValues(proizvodToEdit);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (_db)
+            {
+                return View(_db.Proizvodi.Where(x=>x.Id==id).FirstOrDefault());
+            }
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            using (_db)
+            {
+                return View(_db.Proizvodi.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            using (_db)
+            {
+                var Proizvod=_db.Proizvodi.Where(x => x.Id == id).FirstOrDefault();
+
+                _db.Proizvodi.Remove(Proizvod);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+        }
 
         public ActionResult About()
         {
