4db4a4cb8ab82302e2e67b3fdc890456d5d50077
smalisic <42919793+smalisic@users.noreply.github.com>
2019-04-12 15:12:37 +0200

    Prikaz i dodavanje u bazu

--- ZadatakProizvod/Controllers/HomeController.cs
+++ ZadatakProizvod/Controllers/HomeController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZadatakProizvod.Models;
 
 namespace ZadatakProizvod.Controllers
 {
     public class HomeController : Controller
     {
+        private ProizvodiBazaEntities _db = new ProizvodiBazaEntities();
+
         public ActionResult Index()
         {
+            return View(_db.Proizvodi.ToList());
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude ="Id")]Proizvod proizvodToCreate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _db.Proizvodi.Add(proizvodToCreate);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
